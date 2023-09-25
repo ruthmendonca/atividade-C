5a754d18b32e44cbd31b1cf342cff16642d58898
Ruth Mendonça <99757116+ruthmendonca@users.noreply.github.com>
2023-09-25 17:54:49 -0300

    Update ativ4.c
    Algoritmo para calcular quanto será pago por um produto  (PAG),  sendo  que  o  preço  do  produto  (PR)  e  o  desconto  (D)  são  fornecidos  pelo  usuário.

--- atividades-c/ativ4.c
+++ atividades-c/ativ4.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+	float pr, d, pag;
+	
+	printf("\nDigite o preco do produto: ");
+	scanf("%f", &pr);
+	printf("\n Digite a porcentagem em forma decimal ex: 10% -> 0.10 \n Desconto: ");
+	scanf("%f", &d);
+	
+	pag = pr * (1 - d);
+		
+	printf("\nO preco a ser pago pelo produto com o desconto é: %.2f", pag);
+	
+	return 0;
+}
